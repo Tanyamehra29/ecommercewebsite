0d0362f93024deb30c9e2f9c2f8420f193473393
Tanya Mehra <mehrat375@gmail.com>
2026-07-21 17:47:40 +0530

    Rename Order to TempOrder

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,57 +3,33 @@ import Order from "../models/Order.js";
 import User from "../models/User.js";
 
 export const getDashboardStats = async (req, res) => {
-
   try {
-
     const totalProducts = await Product.countDocuments();
-
     const totalUsers = await User.countDocuments();
-
     const totalOrders = await Order.countDocuments();
 
     const orders = await Order.find();
 
     const totalRevenue = orders.reduce(
-
-      (sum, order) => sum + order.totalPrice,
-
+      (sum, order) => sum + (order.totalPrice || 0),
       0
-
     );
 
     res.status(200).json({
-
       success: true,
-
       stats: {
-
         totalProducts,
-
         totalUsers,
-
         totalOrders,
-
         totalRevenue,
-
       },
-
     });
-
-  }
-
-  catch (error) {
-
-    console.log(error);
+  } catch (error) {
+    console.error(error);
 
     res.status(500).json({
-
       success: false,
-
       message: error.message,
-
     });
-
   }
-
 };
